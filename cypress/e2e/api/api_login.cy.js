ea0330db6cf59b61825645b79cd14baa1cf1e143
Rui Pereira <ruipereira.pf@gmail.com>
2024-11-29 14:25:43 +0000

    asd

--- cypress/e2e/api/api_login.cy.js
+++ cypress/e2e/api/api_login.cy.js
@@ -1,14 +1,32 @@
 /// <reference types="cypress" />
 
-describe('API - Functional Login Test', () => {
-    it('Should login successfully', () =>{
-        cy.request({
-            method: 'POST',
-            url: 'http://localhost:3000/login',
-            body: {
-                "email": "fulano@qa.com",
-                "password": "teste"
-              }
-        })
+describe("API - Functional Login Test", () => {
+  it("Should login successfully", () => {
+    cy.request({
+      method: "POST",
+      url: "http://localhost:3000/login",
+      body: {
+        email: "fulano@qa.com",
+        password: "teste",
+      },
+    }).then((response) => {
+      expect(response.status).to.equal(200);
+      expect(response.body.message).to.equal("Login realizado com sucesso");
     });
+  });
+
+  it("Should validate incorrect password", () => {
+    cy.request({
+      method: "POST",
+      url: "http://localhost:3000/login",
+      body: {
+        email: "fulano@qa.com",
+        password: "wrongpassword",
+      },
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.equal(401);
+      expect(response.body.message).to.equal("Email e/ou senha inválidos");
+    });
+  });
 });
